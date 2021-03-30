0723874a3bccc0f1dbd880a2da9d6590adda614b
Arnold.Grosberg <arnold.grosberg@khk.ee>
2021-03-30 14:02:48 +0300

    Created contacts table and aplication basic menu with Add button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,9 @@
 package sample;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Controller {
+    public void showAddContactDialog(javafx.event.ActionEvent actionEvent) {
+    }
 }
